c459c469733169b5413418c6ecf4825aa82a0f1b
Junaid Iqbal <Junaid.jigar761@gmail.com>
2022-09-30 07:53:35 +0500

    S04.02 DisplayDayForecast : Retrieved data from Intent

--- app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -2,13 +2,25 @@ package com.example.android.sunshine;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class DetailActivity extends AppCompatActivity {
+
+    String mForecast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        Intent intent = getIntent();
+
+        if (intent != null){
+            if (intent.hasExtra(Intent.EXTRA_TEXT)){
+                mForecast = intent.getStringExtra(Intent.EXTRA_TEXT);
+            }
+        }
+
     }
 }
