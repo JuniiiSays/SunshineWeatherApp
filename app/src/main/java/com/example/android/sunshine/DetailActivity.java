d87e3b87b7f6c4d4d9536a7a24361eda67d8d25e
Junaid Iqbal <Junaid.jigar761@gmail.com>
2022-09-30 07:58:19 +0500

    S04.02 DisplayDayForecast : Intent extra is showing in DetailActivity.java

--- app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -4,21 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
-    String mForecast;
+    private String mForecast;
+    private TextView mDisplayWeather;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
+        mDisplayWeather = (TextView) findViewById(R.id.tv_display_weather);
+
         Intent intent = getIntent();
 
         if (intent != null){
             if (intent.hasExtra(Intent.EXTRA_TEXT)){
                 mForecast = intent.getStringExtra(Intent.EXTRA_TEXT);
+                mDisplayWeather.setText(mForecast);
             }
         }
 
